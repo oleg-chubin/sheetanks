b5d4c62d829b53017635475a7c2eb10d7687a3bd
Oleg Chubin <oleg.chubin@gmail.com>
2016-05-21 09:59:05 +0300

    add jinja support

--- sheetanks.py
+++ sheetanks.py
@@ -1,11 +1,21 @@
 from aiohttp import web
+import jinja2
+import aiohttp_jinja2
+import os
 
+
+@aiohttp_jinja2.template('landing_page.html')
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(body=text.encode('utf-8'))
+    return {"name": name}
 
 app = web.Application()
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')
+    )
+)
+
 app.router.add_route('GET', '/{name}', handle)
 
 web.run_app(app)
